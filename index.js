7b85a5c11883ce2e63e1e216639510596b95dc6f
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-08 13:36:49 +0100

    Initiate server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express = require('express');
+const server = express();
+const db = require('./data/db');
+
+server.use(express.json());
+
+server.listen(3000, (req, res) => {
+    console.log('Listening on port 3000');
+})
