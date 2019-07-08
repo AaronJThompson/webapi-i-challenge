073fe14940417abce05e913050299ae312d2e8f9
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-08 15:38:01 +0100

    Add get requests

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const db = require('./data/db');
 
 server.use(express.json());
 
+function handlePromiseResponse(res, prom, error) {
+    prom
+    .then(data => {
+        res.json(data);
+    })
+    .catch(error => {
+        res.status(404)
+        .json({errorMessage:error});
+    });
+}
+server.get('/api/users', (req,res) => {
+    handlePromiseResponse(res, db.find(), "The users information could not be retrieved." );
+})
+
+server.get('/api/users/:id', (req,res) => {
+    handlePromiseResponse(res, db.findById(req.params.id), "The user information could not be retrieved." );
+})
+
 server.listen(3000, (req, res) => {
     console.log('Listening on port 3000');
 })
