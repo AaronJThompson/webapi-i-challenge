dc749cc12968746f6e2cf2d8a30611821e5b7c6c
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-09 14:43:12 +0100

    Large refactor

--- index.js
+++ index.js
@@ -5,22 +5,41 @@ const db = require('./data/db');
 
 server.use(express.json());
 
-function handlePromiseResponse(res, prom, error) {
-    prom
+const getUser = (id, res) => {
+    return new Promise((resolve, reject) => {
+        db.findById(id)
+        .then(users => {
+            if(!user || user.length === 0){
+                reject("User doesn't exist");
+            } else {
+                resolve(users[0]);
+            }
+        })
+        .catch(error => {
+            res.status(404).json({ error: "The user with the specified ID does not exist." });
+            reject(error);
+        })
+    })
+}
+
+server.get('/api/users', (req,res) => {
+    db.find()
     .then(data => {
-        res.json(data);
+        res.status(200).json(data);
     })
     .catch(error => {
-        res.status(404)
-        .json({errorMessage:error});
-    });
-}
-server.get('/api/users', (req,res) => {
-    handlePromiseResponse(res, db.find(), "The users information could not be retrieved." );
+        res.status(500).json({ error: "The users information could not be retrieved." });
+    })
 })
 
 server.get('/api/users/:id', (req,res) => {
-    handlePromiseResponse(res, db.findById(req.params.id), "The user information could not be retrieved." );
+    getUser(req.params.id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(error => {
+        return;
+    });
 })
 
 server.listen(3000, (req, res) => {
